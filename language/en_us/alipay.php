c5351cbb0771d3456f91a8633489759759bc2611
Abdy Franco <abdy@MacBook-Pro.local>
2017-06-27 19:19:33 -0600

    Initial Commit

---
+++ language/en_us/alipay.php
@@ -0,0 +1,13 @@
+<?php
+$lang['Alipay.name'] = 'Alipay';
+$lang['Alipay.merchant_account'] = 'Merchant Account';
+$lang['Alipay.merchant_uid'] = 'Merchant UID/PID';
+$lang['Alipay.signature_key'] = 'Signature Key';
+$lang['Alipay.dev_mode'] = 'Developer Mode';
+$lang['Alipay.dev_mode_note'] = 'Enabling this option will post transactions to the Alipay Sandbox environment. Only enable this option if you are testing with a Alipay Sandbox account.';
+$lang['Alipay.buildprocess.submit'] = 'Pay with Alipay';
+
+// Errors
+$lang['Alipay.!error.merchant_account.valid'] = 'You must enter a valid email address.';
+$lang['Alipay.!error.merchant_uid.valid'] = 'You must enter a valid Merchant UID/PID.';
+$lang['Alipay.!error.signature_key.valid'] = 'You must enter a valid signature key.';
